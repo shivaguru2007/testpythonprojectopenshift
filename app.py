cb166ef2a80235dc4ea29c066b9f62f0beb22335
shiva <shivagurumjar@gmail.com>
2019-07-16 19:25:17 +0530

    xx

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+import os
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return render_template('index.html',user="shiva")
+	
+@application.route("/tamil")
+def hellotamil():
+    return "vanakam"
+	
+
+if __name__ == "__main__":
+	port = int(os.environ.get('PORT', 5000))
+	application.run(host='0.0.0.0',port=port)
